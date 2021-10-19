38e6d0f6d5a80d55fe9899c937d67740bb1db449
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-10-19 12:51:38 +0100

    Add and save Students

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author ryota
+ */
+@Configuration
+public class StudentConfig {
+    
+    @Bean
+            //we add bean so that can be running
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args-> {
+           Student ayoub = new Student(
+                "AyoubKassi",
+                 21,
+                 LocalDate.of(2000,7,2),
+                 "ayoub.kassi@uit.ac.ma"
+            );
+           Student maroua = new Student(
+    
+                "MarouaNufi",
+                 21,
+                 LocalDate.of(2000,5,13),
+                 "maroua.elazzaoui@uit.ac.ma"
+            );
+           
+           repository.saveAll(
+                   List.of(ayoub,maroua)
+           );
+        };
+    }
+}
