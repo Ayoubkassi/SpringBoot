3fb41cebb148b47117e44820ba8fa4e7b9bc6f17
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-10-19 13:10:51 +0100

    PostMapping for Creation of new student

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -8,6 +8,8 @@ package com.example.demo.student;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,11 @@ public class StudentController {
       public List<Student> getStudents(){
           return studentService.getStudents();
       }
+      
+      @PostMapping
+      public void registerNewStudent(@RequestBody Student student){
+          studentService.addNewStudent(student);
+      }
+      
       
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -32,5 +32,7 @@ public class StudentService{
             return studentRepository.findAll();
         }
     
-   
+   public void addNewStudent(Student student){
+       System.out.println(student);
+   } 
 }
